e89a024ca7d648a16fef7fe5a59c18ff25b6c28c
Sravya1011 <64693313+Sravya1011@users.noreply.github.com>
2023-04-01 14:58:34 +0530

    Update Constants.cs

--- src/WebAppSSLManager/Models/Constants.cs
+++ src/WebAppSSLManager/Models/Constants.cs
@@ -7,7 +7,7 @@ namespace WebAppSSLManager.Models
         public const string AppPropertiesFileName = "appproperties.json";
         public const string CertInfoFileName = "certinfo.json";
         public const string AccountKeyFileName = "accountkey.pem";
-        public const string DefaultCertificatePassword = "C0mpl1c4t3d57r1ng";
+        public const string DefaultCertificatePassword = "C0mpl1c4t8d57r1ng";
         public const string CertificateBlobContainer = "certificates";
         public const string DefaultEmailSender = "AzureWebAppSSLManager@dbtek.com.hk";
         public const string DefaultCA = "Let's Encrypt Authority";
